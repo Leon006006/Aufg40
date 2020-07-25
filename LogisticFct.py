ea8a55d37976182513b6e063d63e93e414f07d39
Leon006006 <brasselerle73295@th-nuernberg.de>
2020-07-25 10:42:22 +0200

    Finished Version

--- LogisticFct.py
+++ LogisticFct.py
@@ -2,29 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def logisticFct(r, x):
-    return r * x*(1 - x)
+def logisticFct(reproduction_factor, x):
+    return reproduction_factor * x * (1 - x)
 
 
-def log_fix(r, start):
-    x = start
+def log_fix(reproduction_factor, start_pop):
+    """
+    :param reproduction_factor: Reproduction factor r for logistic function
+    :param start_pop: Starting population
+    :return: Points 2000 to 2200 of the Iteration
+    """
+    x = start_pop
+    # Compute first 2000 steps
+    # Fixed points should be reached
     for iteration in range(2000):
-        x = logisticFct(r, x)
-    fix_x = np.zeros(200)
+        x = logisticFct(reproduction_factor, x)
+
+    # Compute next "fix_p" steps for plot
+    fix_x = np.zeros(fix_p)
     fix_x[0] = x
-    for iteration in range(199):
-        fix_x[iteration + 1] = logisticFct(r, fix_x[iteration])
+    for iteration in range(fix_p-1):
+        fix_x[iteration + 1] = logisticFct(reproduction_factor, fix_x[iteration])
     return fix_x
 
 
-r_vec = np.linspace(1, 4, 1000)
-plot_matrix = np.zeros((200, 1000))
-counter = 0
+# Initialize Constants
+fix_p = 200
+count_r = 300
+r_start = 2.9
+r_end = 4
+x_start = 0.5
 
+r_vec = np.linspace(r_start, r_end, count_r)
+plot_matrix = np.zeros((fix_p, count_r))
+
+# Iterate over different reproduction values r
+counter = 0
 for r in r_vec:
-    plot_matrix[:, counter] = log_fix(r, 0.5)
-    plt.plot(r_vec[counter] * np.full(200, 1), plot_matrix[:, counter], 'bo', markersize=1)
+    plot_matrix[:, counter] = log_fix(r, x_start)
+    plt.plot(r_vec[counter] * np.full(fix_p, 1), plot_matrix[:, counter], 'bo', markersize=1)
     counter += 1
+
+# PLot Data
 plt.title('Fix points with x=0.5 as staring point')
 plt.xlabel('r')
 plt.ylabel('Fixed point')
